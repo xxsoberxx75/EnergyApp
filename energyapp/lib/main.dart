742389f41abfe969160a7829d8c736d30be6430c
xxsoberxx75 <koichinjulie@gmail.com>
2025-11-21 08:28:31 +0400

    test101

--- energyapp/lib/main.dart
+++ energyapp/lib/main.dart
@@ -4,9 +4,22 @@ import 'package:energyapp/tips.dart';
 import 'package:flutter/material.dart';
 import 'dashboard_page.dart';
 import 'features/quiz/screens/quiz_screen.dart';
-// import 'pages/tips_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // 🔥 Initialize Firebase for Web (Chrome & Edge)
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyAjra3Z8HfcaMQS-KKXoRISJUiQLHrgprY",
+      appId: "1:570566659864:web:5a9ff53774a44b7448586d",
+      messagingSenderId: "570566659864",
+      projectId: "energyapp-66c13",
+      storageBucket: "energyapp-66c13.firebasestorage.app",
+    ),
+  );
+
   runApp(const EnergyApp());
 }
 
